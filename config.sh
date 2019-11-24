e9398f1e28883bd9b25bb1db37fb7097ab78a16c
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-11-24 15:19:47 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-GITHUB_USER=timangpopi
-GITHUB_EMAIL=timangpopi@gmail.com
+GITHUB_USER="timangpopi@gmail.com"
+GITHUB_EMAIL=timangpopi
 
-KBUILD_BUILD_USER="Timangpopi"
-KBUILD_BUILD_HOST="A_DEAD_PLANET"
+KBUILD_BUILD_USER="timangpopi"
+KBUILD_BUILD_HOST="timangpopi"
 
 export oem=xiaomi
 export device=rolex
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
+ROM="lineage-16.0"
+manifest_url="https://github.com/LineageOS/android.git"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+branch="lineage-16.0"
 
-release_repo="timangpopi/awowkwk"
+release_repo="timangpopi/build"
 
 timezone="Asia/Jakarta"
+
+export TELEGRAM_TOKEN="923829062:AAHAkKpoL-iAdDm6nmp4GPjKhjfxWkTMLPY"
+export TELEGRAM_CHAT="-1001158707255"
+export GITHUB_TOKEN="923829062:AAHAkKpoL-iAdDm6nmp4GPjKhjfxWkTMLPY"
