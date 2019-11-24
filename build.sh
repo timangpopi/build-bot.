2f8767377f48f9f9f38972095e3e8f63919ca83c
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-11-24 15:33:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,42 +28,60 @@ function trim_darwin() {
     cat manifest.xml | grep -v darwin  >temp  && cat temp >manifest.xml  && rm temp
     cd ../
 
+export outdir="out/target/product/$device"
+
 # Now Compile
+
 mkdir "$ROM"
 cd "$ROM"
 
-export outdir="out/target/product/$device"
+echo "Sync started for "$manifest_url""
+../telegram -M "Sync Started for ["$ROM"]("$manifest_url")"
+SYNC_START=$(date +"%s")
+#trim_darwin >/dev/null   2>&1
+#repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q 2>&1 >>logwe 2>&1
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags #2>&1 >>logwe 2>&1
+if [ -e device/$oem/$device ]; then
 
-# Initialize repo
-repo init -u "$manifest_url" -b "$branch" --depth 1 &> /dev/null
-trim_manifest &> /dev/null
-echo "Sync started for $manifest_url"
-#telegram -M "Sync Started for [$ROM]($manifest_url)"
-
-# Reset bash timer and begin syncing
-SECONDS=0
-if repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) &> /dev/null; then
-	# Syncing completed, clone custom repos if any
-	bash ./clone.sh
-
-	echo -e "\n\nSync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
+else
+	bash ../clone.sh
+fi
+SYNC_END=$(date +"%s")
+SYNC_DIFF=$((SYNC_END - SYNC_START))
+if [ -e frameworks/base ]; then
+    echo "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
     echo "Build Started"
-    #telegram -M "Sync completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)
+    ../telegram -M "Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds"
 
-#Build Started: [See Progress]($ci_url)"
-	
-	# Reset bash timer and begin compilation
-    SECONDS=0
-    source build/envsetup.sh &> /dev/null
-    if [ -e "device/$oem/$device" ]; then python3 /drone/src/dependency_cloner.py; fi
-    lunch $rom_vendor_name_$device-userdebug &> /dev/null
+    ../telegram -M "Build Started
+ROM : ""$ROM""
+Android : "$branch"
+Device : "$device"
+Brand : "$oem"
+Type : UNOFFICIAL
+Dev : ""$KBUILD_BUILD_USER""
+Build Date : ""$(env TZ=$timezone date)""
+"
 
-    if mka bacon | grep $device; then
-		# Build completed succesfully, upload it to github
-		export finalzip_path=$(ls "$outdir/*2019*.zip" | tail -n -1)
-		export zip_name=$(basename -s "$finalzip_path")
-        echo -e "\n\nBuild completed successfully in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
-        echo "Uploading..."
+    BUILD_START=$(date +"%s")
+
+    source build/envsetup.sh >/dev/null  2>&1
+    source ../config.sh
+    if [ -e device/"$oem"/"$device" ]; then
+        python3 ../dependency_cloner.py
+    fi
+    lunch "$rom_vendor_name"_"$device"-userdebug >/dev/null  2>&1
+    make bacon -j4
+    BUILD_END=$(date +"%s")
+    BUILD_DIFF=$((BUILD_END - BUILD_START))
+
+    export finalzip_path=$(ls "$outdir"/*201*.zip | tail -n -1)
+    export zip_name=$(echo "$finalzip_path" | sed "s|"$outdir"/||")
+    export tag=$( echo "$zip_name" | sed 's|.zip||')
+    if [ -e "$finalzip_path" ]; then
+        echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
+
+        echo "Uploading"
 
         #github-release "$release_repo" "$zip_name" "master" "$ROM for $device
 
@@ -87,4 +105,4 @@ else
     #telegram -N -M "ALERT: Sync failed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)"
     exit 1
 fi
-#End build
+echo "end build"
