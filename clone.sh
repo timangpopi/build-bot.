6a81e77972188644604e7f9ce0bfac2d30c345a4
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-11-24 14:31:36 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# clone repos for PixelExperience 
 
-# write commands if you want to clone some repos
+GITHUB='https://github.com/Nick89786'
+BRANCH="lineage-16.0"
+
+git clone -b $BRANCH $GITHUB/android_device_xiaomi_rolex device/xiaomi/rolex
+git clone -b $BRANCH $GITHUB/android_vendor_xiaomi vendor/xiaomi/rolex
+git clone -b $BRANCH $GITHUB/android_kernel_xiaomi_msm8917 kernel/xiaomi/msm8917
