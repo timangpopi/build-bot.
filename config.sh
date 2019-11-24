9072476389465c2914160d88688dc806fcccd4e8
timangpopi <53063799+timangpopi@users.noreply.github.com>
2019-11-24 14:19:48 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=timangpopi
+GITHUB_EMAIL=timangpopi@gmail.com
 
-KBUILD_BUILD_USER="Aki"
+KBUILD_BUILD_USER="Timangpopi"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=xiaomi
+export device=rolex
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="Akianonymus/harpia_builds"
+release_repo="timangpopi/awowkwk"
 
-timezone="Asia/Kolkata"
+timezone="Asia/Jakarta"
